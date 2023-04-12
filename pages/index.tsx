349d11bab1e66fc5abc01f2f1c148d8300951100
Sato-07 <120731915+Sato-07@users.noreply.github.com>
2023-04-12 23:09:09 +0200

    feat: Add server-side authentication check using Next.js getSession()

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,32 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
+import { getSession, signOut } from 'next-auth/react'
+import { NextPageContext } from 'next'
+
+export async function getServerSideProps(context:NextPageContext){
+  const session = await getSession(context)
+  if(!session){
+    return{
+      redirect:{
+        destination:'/auth',
+        permanent:false,
+      }
+    }
+  }
+  return{
+    props:{
+      session
+    }
+  }
+}
+
 
 export default function Home() {
   return (
     <>
     <h1 className="text-2xl text-red-500">Netflix clone</h1>
+    <button className='h-10 w-full bg-white' onClick={()=> signOut()}>Logout! </button>
     </>
   )
 }
